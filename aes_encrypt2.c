b764e26a3bc6ccb5e97b47922b0af5203da16f4c
xerri <83772595+xerri04@users.noreply.github.com>
2023-08-10 17:13:29 +0800

    Update aes_encrypt2.c

--- aes_encrypt2.c
+++ aes_encrypt2.c
@@ -45,32 +45,53 @@ int main(void)
         return 1;
     }
 
+    uint8_t i;
+    char MESSAGE[CHUNK_SIZE];                               
     char key[32];
-    char nonce[16]; 
-    uint8_t MESSAGE[CHUNK_SIZE];
-    uint8_t i;                               
+    char nonce[16];
 
-    randombytes_buf(nonce, sizeof(nonce));
+    fseek(fp_in, 0, SEEK_END);
+    int mlen = ftell(fp_in);
+    fseek(fp_in, 0, SEEK_SET);
 
-    size_t klen = fread(key, 1, sizeof(key), PMK_Key);
+    randombytes_buf(nonce, 16);
+    int nlen = strlen(nonce);
     fwrite(nonce, sizeof(char), sizeof(nonce), fp_out);
 
+    fread(key, 1, 32, PMK_Key);
+    int klen = strlen(key);
+    
     printf("key:\n");
-    phex((const uint8_t *)key);
+    phex(key);
     printf("\n");
 
     printf("nonce:\n");
-    phex((const uint8_t *)nonce);
+    phex(nonce);
     printf("\n");
 
-    // Proper length of key and nonce
-    int klenu = 32;
-    int nlenu = 16;
+    // Proper length of message, key and nonce
+    int mlenu = mlen;
+    if (mlen % 16) {
+        mlenu += 16 - (mlen % 16);
+        printf("The original length of the STRING = %d and the length of the padded STRING = %d\n", mlen, mlenu);
+    }
+
+    int klenu = klen;
+    if (klen % 16) {
+        klenu += 16 - (klen % 16);
+        printf("The original length of the KEY = %d and the length of the padded KEY = %d\n", klen, klenu);
+    }
+
+    int nlenu = nlen;
+    if (nlen % 16) {
+        nlenu += 16 - (nlen % 16);
+        printf("The original length of the NONCE = %d and the length of the padded NONCE = %d\n", nlen, nlenu);
+    }
 
     // Make the uint8_t arrays
-    uint8_t hexarray[CHUNK_SIZE];
-    uint8_t kexarray[32];
-    uint8_t nexarray[16];
+    uint8_t hexarray[mlenu];
+    uint8_t kexarray[klenu];
+    uint8_t nexarray[nlenu];
     
     // Initialize them with zeros
     memset( hexarray, 0, sizeof(hexarray) );
@@ -78,34 +99,38 @@ int main(void)
     memset( nexarray, 0, sizeof(nexarray) );
     
     // Fill the uint8_t arrays
-    for (size_t i = 0; i < CHUNK_SIZE; i++) {
-        hexarray[i] = MESSAGE[i];
+    for (int i = 0; i < mlen; i++) {
+        hexarray[i] = (uint8_t)MESSAGE[i];
     }
 
-    for (size_t i = 0; i < klen; i++) {
+    for (int i = 0; i < klen; i++) {
         kexarray[i] = (uint8_t)key[i];
     }
     
-    for (size_t i = 0; i < sizeof(nonce); i++) {
+    for (int i = 0; i < nlen; i++) {
         nexarray[i] = (uint8_t)nonce[i];
     }
 
+    int messagePad = pkcs7_padding_pad_buffer( hexarray, mlen, sizeof(hexarray), 16 );
+    int keyPad = pkcs7_padding_pad_buffer( kexarray, klen, sizeof(kexarray), 16 );
+    int noncePad = pkcs7_padding_pad_buffer( nexarray, nlen, sizeof(nexarray), 16 );
+    
+    // In case you want to check if the padding is valid
+    int valid = pkcs7_padding_valid( hexarray, mlen, sizeof(hexarray), 16 );
+    int valid2 = pkcs7_padding_valid( kexarray, klen, sizeof(kexarray), 16 );
+    int valid3 = pkcs7_padding_valid( nexarray, nlen, sizeof(nexarray), 16 );
+    printf("Is the pkcs7 padding valid message = %d  |  key = %d  |  nonce = %d\n", valid, valid2, valid3);
+
     size_t bytes_read;
 
-    while ((bytes_read = fread(MESSAGE, 1, CHUNK_SIZE, fp_in)))
+    while ((bytes_read = fread(hexarray, 1, CHUNK_SIZE, fp_in)))
     {
-        // Proper length of message
-        size_t mlenu = bytes_read;
-        if (bytes_read % 16) {
-            mlenu += 16 - (bytes_read % 16);
-        }
-
         // Start the encryption
-        AES_init_ctx_iv(&ctx, kexarray, nexarray);
-        
+        AES_init_ctx_iv(&ctx, (const uint8_t *)kexarray, (const uint8_t *)nexarray);
+
         // Encrypt
-        AES_CBC_encrypt_buffer(&ctx, MESSAGE, mlenu);
-        fwrite(MESSAGE, 1, mlenu, fp_out);
+        AES_CBC_encrypt_buffer(&ctx, (uint8_t *)hexarray, mlenu);
+        fwrite(hexarray, 1, mlenu, fp_out);
     }
 
     fclose(fp_in);
